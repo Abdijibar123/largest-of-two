a35309a250241af4b7ae762d6f9b67f793f48054
Abdijibar123 <abdijibar1313@gmail.com>
2023-05-29 11:40:56 +0300

    done exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+a = 20
+b = 10
+if(a > b){
+    console.log(`${a} isgreater than ${b} `)
+}
+else if(a < b){
+    console.log(`${b} is greater than ${a} `)
+}
+else{
+    console.log(`the two numbers are equal`)
+}
